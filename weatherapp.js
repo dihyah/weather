bfe0c95af393e2ef95bd4bde33172c01862df200
dihyah <hiidihyah@gmail.com>
2021-05-07 14:19:26 +0800

    update files via upload

--- weatherapp.js
+++ weatherapp.js
@@ -14,14 +14,16 @@ function event(evt){
     getWeatherReport(searchBox.value);
   }
 }
-//Get Weather Report
+
+//Parse URL for Weather Report
 function getWeatherReport(city){
 	fetch(`${api.base}weather?q=${city}&appid=${api.key}&units=metric`)
 	.then(weather =>{
 		return weather.json();
 	}).then(showWeatherReport);
 }
-//Show Weather Report
+
+//Extract JSON Data and Show Weather Report
 function showWeatherReport(weather){
 	console.log(weather);
 
@@ -32,20 +34,22 @@ function showWeatherReport(weather){
 	temperature.innerHTML = `${Math.round(weather.main.temp)}&deg;C`;
 
 	let minmaxTemp = document.getElementById("min-max");
-	minmaxTemp.innerHTML = `${Math.round(weather.main.temp_min)}&deg;C / ${Math.round(weather.main.temp_max)}&deg;C`;
+	minmaxTemp.innerHTML = `${Math.round(weather.main.temp_min)}&deg;C(min) / ${Math.round(weather.main.temp_max)}&deg;C(max)`;
 
 	let weatherType = document.getElementById("weather");
 	weatherType.innerText = weather.weather[0].main;
+    console.log(weather.weather[0].main)
 
 	let date = document.getElementById("date");
 	let todayDate = new Date();
 	date.innerText = dateManage(todayDate);
 
+    //changeable icon
 	let locationIcon = document.querySelector('.weather-icon');
 	var icon = ("<img src='icon/" + weather.weather[0].icon + ".png'>");
 	locationIcon.innerHTML = icon;
 
-	//change background
+	//changeable background
 	if(weatherType.textContent == "Clear"){
 	   document.body.style.backgroundImage = "url('img/sunnybg.jpg')";
 
@@ -66,8 +70,8 @@ function showWeatherReport(weather){
     }
 	
 }
-//Date Manage
 
+//Date Manage
 function dateManage (dateArg) {
   let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
   
@@ -82,11 +86,32 @@ function dateManage (dateArg) {
 }
 
 //Time Zone
-function startClock(){
-  setInterval(function(){
-    $("#localTime").text(new Date().toLocaleTimeString());
-  }, 1000);
-}
+(function startClock(){
+        setInterval(function(){
+            $("#localTime").text(new Date().toLocaleTimeString());
+            $("#localClock").text(new Date().toLocaleTimeString());
+        }, 1000);
+    })();
 
-startClock();
+//Obtain Geolocationt
+navigator.geolocation.getCurrentPosition(
+      function(pos) {
+        $("#lat_field").val(pos.coords.latitude);
+        $("#long_field").val(pos.coords.longitude);
+      }
+    );
+
+var geocoder = new google.maps.Geocoder();
+var latLng = new google.maps.LatLng(41.03531125,29.0124264);
+
+if (geocoder) {
+  geocoder.geocode({ 'latLng': latLng}, function (results, status) {
+     if (status == google.maps.GeocoderStatus.OK) {
+        console.log(results[0].formatted_address);
+     }
+     else {
+        console.log("Geocoding failed: " + status);
+     }
+  });
+}
 
